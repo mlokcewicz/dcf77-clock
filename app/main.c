6196cb1c94220f0ff19c10ec30c0245f0ad5e827
mlokcewicz <mlokcewicz@gmail.com>
2025-03-24 12:53:50 +0100

    Update main.c

--- app/main.c
+++ app/main.c
@@ -389,16 +389,16 @@ int main()
 // ISP - pod ISP
 
 // Basic threads / processess:
-// * radio_manager (waits for request and TIM1 interrupts)
-// * ui_manager (waits for INT0, PCINT interrupts from encoder / button)
-// * clock_manager (waits for SQW and send event to UI)
-// * main (transfers event)
+// * radio_manager (waits for request of sync, then enables dcf receiver and TIM1 irq, and set new last_sync timestamp, and waits for sync_finished and disables dcf and TIM1 irq)
+// * clock_manager (waits for SQW and send event to UI, wait for time from TIM1 irq or from UI)
+// * ui_manager (waits for INT0, PCINT interrupts from encoder / button and driver LCD in thread, waits for SWQ irq to update time, checks for alert)
 
 // Events:
-// * ENC+ (to ui_manager)
-// * ENC- (to ui_manager)
-// * SW (to ui_manager)
-// * SEC (to clock_manager)
-// * Sync REQ (from ui_manager or radio_manager) (to radio_manager)
-// * Clock UPDT REQ (from ui_manager or radio_manager) (to clock_manager or ui_manager)
+// * **ENC+ (to ui_manager)
+// * **ENC- (to ui_manager)
+// * **SW (to ui_manager)
+// * **TIM (to clock_manager/radio_manager)
+// * **SEC (to clock_manager/ui_manager)
+// * Sync REQ (from ui_manager and radio_manager) (to radio_manager)
+// * Clock UPDT REQ (from ui_manager and radio_manager) (to clock_manager and ui_manager)
 // * Alarm (from clock_manager) (to ui_manager)
