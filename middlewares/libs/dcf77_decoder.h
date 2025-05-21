61826e79018fd5cae554cb7f36f8cfc012f6215e
mlokcewicz <mlokcewicz@gmail.com>
2025-05-21 18:38:39 +0200

    Cleanup header file

--- middlewares/libs/dcf77_decoder.h
+++ middlewares/libs/dcf77_decoder.h
@@ -32,31 +32,6 @@ enum dcf77_decoder_status
 //------------------------------------------------------------------------------
 // Macros for extracting DCF77 frame fields from a uint8_t* frame (bit 0 = LSB of frame[0])
 
-// MOJE 96% - Program:    7868 bytes (96.0% Full)
-
-// #define DCF77_DECODER_FRAME_GET_FRAME_START(frame)         (((frame[0] >> 0) & 0x01))
-// #define DCF77_DECODER_FRAME_GET_WEATHER_INFO(frame)        ((((uint16_t)(frame[0] >> 1)) & 0x7F) | (((uint16_t)(frame[1] & 0x3F)) << 7))
-// #define DCF77_DECODER_FRAME_GET_AUX_ANTENNA(frame)         (((frame[1] >> 7) & 0x01))
-// #define DCF77_DECODER_FRAME_GET_TIME_CHANGE_ANN(frame)     (((frame[2] >> 0) & 0x01))
-// #define DCF77_DECODER_FRAME_GET_WINTER_TIME(frame)         (((frame[2] >> 1) & 0x03))
-// #define DCF77_DECODER_FRAME_GET_LEAP_SECOND(frame)         (((frame[2] >> 3) & 0x01))
-// #define DCF77_DECODER_FRAME_GET_TIME_START(frame)          (((frame[2] >> 4) & 0x01))
-
-// #define DCF77_DECODER_FRAME_GET_MINUTES_UNITS(frame)       (((frame[3] >> 0) & 0x01) << 3 | ((frame[2] >> 5) & 0x07))
-// #define DCF77_DECODER_FRAME_GET_MINUTES_TENS(frame)        (((frame[3] >> 1) & 0x07))
-// #define DCF77_DECODER_FRAME_GET_MINUTES_PARITY(frame)      (((frame[3] >> 4) & 0x01))
-// #define DCF77_DECODER_FRAME_GET_HOURS_UNITS(frame)         (((frame[4] >> 0) & 0x01) << 3 | ((frame[3] >> 5) & 0x07))
-// #define DCF77_DECODER_FRAME_GET_HOURS_TENS(frame)          (((frame[4] >> 1) & 0x03))
-// #define DCF77_DECODER_FRAME_GET_HOURS_PARITY(frame)        (((frame[4] >> 3) & 0x01))
-// #define DCF77_DECODER_FRAME_GET_DAY_UNITS(frame)           (((frame[4] >> 4) & 0x0F))
-// #define DCF77_DECODER_FRAME_GET_DAY_TENS(frame)            (((frame[5] >> 0) & 0x03))
-// #define DCF77_DECODER_FRAME_GET_WEEKDAY(frame)             (((frame[5] >> 2) & 0x07))
-// #define DCF77_DECODER_FRAME_GET_MONTH_UNITS(frame)         (((frame[6] >> 0) & 0x01) << 3 | ((frame[5] >> 5) & 0x07))
-// #define DCF77_DECODER_FRAME_GET_MONTH_TENS(frame)          (((frame[6] >> 1) & 0x01))
-// #define DCF77_DECODER_FRAME_GET_YEAR_UNITS(frame)          (((frame[6] >> 2) & 0x0F))
-// #define DCF77_DECODER_FRAME_GET_YEAR_TENS(frame)           (((frame[7] >> 0) & 0x03) << 2 | ((frame[6] >> 6) & 0x03))
-// #define DCF77_DECODER_FRAME_GET_DATE_PARITY(frame)         (((frame[7] >> 2) & 0x01))
-
 #define DCF77_GET_BITS(frame, bit_pos, mask) ((((((uint16_t)frame[((bit_pos) / 8) + 1] << 8) | (uint16_t)frame[(bit_pos) / 8]) >> ((bit_pos) % 8)) & (mask)))
 
 #define DCF77_DECODER_FRAME_GET_FRAME_START(frame)          DCF77_GET_BITS(frame, 0, 0x01)
