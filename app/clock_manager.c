18948906cea10863955027134186b236b3cd752b
mlokcewicz <mlokcewicz@gmail.com>
2025-05-22 00:00:18 +0200

    Add time shifting by timezone

--- app/clock_manager.c
+++ app/clock_manager.c
@@ -16,13 +16,13 @@
 #define CLOCK_MANAGER_TIMESTAMP(h, m) (struct hal_timestamp){.hours = h, .minutes = m}
 
 #define CLOCK_MANAGER_SYNC_TIMESTAMP CLOCK_MANAGER_TIMESTAMP(4, 0)
+#define CLOCK_MANAGER_DCF77_TIME_ZONE 1
 
 //------------------------------------------------------------------------------
 
 struct clock_manager_ctx
 {
     bool new_sec;
-    struct hal_timestamp alarm;
 };
 
 static struct clock_manager_ctx ctx;
@@ -45,15 +45,71 @@ static bool time_is_equal(event_update_time_req_data_t *time, struct hal_timesta
             time->hours == timestamp->hours);
 }
 
+static const uint8_t days[] = {31,28,31,30,31,30,31,31,30,31,30,31};
+
+static bool is_leap_year(uint8_t year)
+{
+    year += 2000;
+    return ((year % 4 == 0) && ((year % 100 != 0) || (year % 400 == 0)));
+}
+
+static uint8_t days_in_month(uint8_t m, uint8_t y)
+{
+    if (m == 2 && is_leap_year(y)) return 29;
+    if (m >= 1 && m <= 12) return days[m-1];
+    return 31;
+}
+
+static void shift_time(event_set_time_req_data_t *t, int8_t tz)
+{
+    int8_t h = t->hours + tz;
+    int8_t d = t->date, m = t->month, y = t->year;
+
+    if (h < 0)
+    {
+        h += 24;
+        d--;
+    }
+    else if (h >= 24)
+    {
+        h -= 24;
+        d++;
+    }
+    t->hours = h;
+
+    uint8_t dim = days_in_month(m, y);
+
+    if (d < 1)
+    {
+        if (--m < 1)
+        {
+            m = 12; /*y = (y ? y-1 : 99);*/
+        }
+        d = days_in_month(m, y);
+    }
+    else if (d > dim)
+    {
+        d = 1;
+        if (++m > 12)
+        {
+            m = 1; /*y = (y < 99 ? y+1 : 0);*/
+        }
+    }
+
+    t->date = d;
+    t->month = m;
+    // t->year = y;
+}
+
 //------------------------------------------------------------------------------
 
 bool clock_manager_init(void)
 {
-    // if (hal_time_is_reset())
-    //     event_set(EVENT_SYNC_TIME_REQ);
+    /* if (hal_time_is_reset()) */
+        event_set(EVENT_SYNC_TIME_REQ);
 
-    hal_get_alarm(&ctx.alarm);
     hal_get_alarm(event_get_data(EVENT_SET_ALARM_REQ));
+    hal_get_timezone(event_get_data(EVENT_SET_TIME_ZONE_REQ));
 
     return true;
 }
@@ -62,15 +118,31 @@ void clock_manager_process(void)
 {
     if (event_get() & EVENT_SET_TIME_REQ)
     {
+        event_set_time_req_data_t *time = event_get_data(EVENT_SET_TIME_REQ);
+
+        /* Set time request without set time zone request means DCF77 sync request - shift from UTC+01 needed */
+        if (!(event_get() & EVENT_SET_TIME_ZONE_REQ))
+        {
+            int8_t *time_zone = event_get_data(EVENT_SET_TIME_ZONE_REQ);
+            
+            shift_time(time, *time_zone - CLOCK_MANAGER_DCF77_TIME_ZONE); 
+        }
+
         hal_set_time(event_get_data(EVENT_SET_TIME_REQ));
 
         event_clear(EVENT_SET_TIME_REQ);
     }
 
+    if (event_get() & EVENT_SET_TIME_ZONE_REQ)
+    {
+        hal_set_timezone(event_get_data(EVENT_SET_TIME_ZONE_REQ));
+
+        event_clear(EVENT_SET_TIME_ZONE_REQ);
+    }
+
     if (event_get() & EVENT_SET_ALARM_REQ)
     {
         hal_set_alarm(event_get_data(EVENT_SET_ALARM_REQ));
-        hal_get_alarm(&ctx.alarm);
 
         event_clear(EVENT_SET_ALARM_REQ);
     }
@@ -78,19 +150,18 @@ void clock_manager_process(void)
     if (ctx.new_sec)
     {
         event_update_time_req_data_t *time = event_get_data(EVENT_UPDATE_TIME_REQ);
+        event_set_alarm_req_data_t *alarm = event_get_data(EVENT_SET_ALARM_REQ);
 
         hal_get_time(time);
 
         event_set(EVENT_UPDATE_TIME_REQ);
 
+        /* TODO: Change timestamp check to >= and add timestamp_handled flag */
         if (time_is_equal(time, &CLOCK_MANAGER_SYNC_TIMESTAMP))
             event_set(EVENT_SYNC_TIME_REQ);
 
-        if (ctx.alarm.is_enabled && time_is_equal(time, &ctx.alarm))
-        {
+        if (alarm->is_enabled && time_is_equal(time, alarm))
             event_set(EVENT_ALARM_REQ);
-            ctx.alarm.is_enabled = false;
-        }
 
         ctx.new_sec = false;
     }
